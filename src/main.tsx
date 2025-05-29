b1d734c80689e6d73f2f707e5ecd8e46df44c8f0
Hanson <hansenhs21@live.com>
2025-05-29 19:50:19 +0800

    feat: add vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.tsx'
 
@@ -8,5 +9,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
